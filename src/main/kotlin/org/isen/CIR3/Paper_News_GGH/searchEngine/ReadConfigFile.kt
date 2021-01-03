9fdff1e740efec8a134dc81f0ca4aa48a79a1d1b
BrSkat <50581350+BrSkat@users.noreply.github.com>
2021-01-03 10:53:39 +0100

    Update ReadConfigFile.kt

--- src/main/kotlin/org/isen/CIR3/Paper_News_GGH/searchEngine/ReadConfigFile.kt
+++ src/main/kotlin/org/isen/CIR3/Paper_News_GGH/searchEngine/ReadConfigFile.kt
@@ -1,18 +1,20 @@
 package org.isen.CIR3.Paper_News_GGH.searchEngine
 
 import com.sksamuel.hoplite.ConfigLoader
+import org.apache.logging.log4j.kotlin.Logging
 import org.isen.CIR3.Paper_News_GGH.data.ConfigData
 
 class ReadConfigFile {
+    companion object : Logging
     val cfg:ConfigData
 
     init {
-        NewsSearchEngine.logger.info("---------------------------------config file reading-------------------------------------------")
+        logger.info("---------------------------------config file reading-------------------------------------------")
         this.cfg= ConfigLoader().loadConfigOrThrow("/config.yaml")
-        NewsSearchEngine.logger.info("api key found : ${cfg.apiKey}")
-        NewsSearchEngine.logger.info("default country found : ${cfg.defaultLanguage}")
-        NewsSearchEngine.logger.info("category list found : ${cfg.categoryList}")
-        NewsSearchEngine.logger.info("country list found : ${cfg.countryList}")
-        NewsSearchEngine.logger.info("-----------------------------------------------------------------------------------------------")
+        logger.info("api key found : ${cfg.apiKey}")
+        logger.info("default country found : ${cfg.defaultLanguage}")
+        logger.info("category list found : ${cfg.categoryList}")
+        logger.info("country list found : ${cfg.countryList}")
+        logger.info("-----------------------------------------------------------------------------------------------")
     }
 }
